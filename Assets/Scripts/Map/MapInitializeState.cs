6873a98af93d0b5ea572b3f5d8b5e89ff34c964b
noa_yamashita <t.yamashita@noa-tec.co.jp>
2022-02-20 16:22:51 +0900

    棘の鎧実装

--- Assets/Scripts/Map/MapInitializeState.cs
+++ Assets/Scripts/Map/MapInitializeState.cs
@@ -54,9 +54,9 @@ public class MapInitializeState : StateBase {
 
 		// TODO アーティファクト効果テスト
 		// ここに、最初からアーティファクトを持たせて、効果を発揮できるようにする
-		MasterArtifactTable.Data data = MasterArtifactTable.Instance.GetData(6);
+		MasterArtifactTable.Data data = MasterArtifactTable.Instance.GetData(7);
 		scene.AddArtifactObject(data);
-		MapDataCarrier.Instance.RemoveRarityNoAcquiredArtifactList(6);
+		MapDataCarrier.Instance.RemoveRarityNoAcquiredArtifactList(7);
 
 		//data = MasterArtifactTable.Instance.GetData(2);
 		//scene.AddArtifactObject(data);
